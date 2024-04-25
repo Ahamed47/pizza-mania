7b9eccf27ffbeb5309f3d7613fac6e4d905fea9e
Ahamed Abdul Khader A <ahamedabdul24@gmail.com>
2024-04-25 18:26:57 +0530

    Route Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -7,8 +8,10 @@ import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>
 );

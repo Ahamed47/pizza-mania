fe5bac49a684b37e48243df9dfeb4e473fbd8124
Ahamed Abdul Khader A <ahamedabdul24@gmail.com>
2024-04-25 18:37:17 +0530

    route git fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+//import { HashRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store';
+// import { Provider } from 'react-redux';
+// import store from './store';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </HashRouter>
+//   </React.StrictMode>
+// );
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

7a136de89cb10691b20a14062cc103e078475144
Ahamed Abdul Khader A <ahamedabdul24@gmail.com>
2024-04-25 19:03:29 +0530

    Final route fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import { HashRouter } from 'react-router-dom';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
-// import { Provider } from 'react-redux';
-// import store from './store';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <HashRouter>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     </HashRouter>
-//   </React.StrictMode>
-// );
-
-const router = createHashRouter([
-  {
-    path: '/*',
-    element: <App />,
-  },
-]);
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
